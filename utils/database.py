3b10adda6f2ba82ec03a9bdd40dafefac9713a36
RadoslawDrab <radoslaw.drab03@gmail.com>
2026-05-28 11:39:16 +0200

    ``` feat: enhance database handling with in-memory option (issue #123)
    - Add support for in-memory data handling using io.StringIO.
    - Introduce 'persistent' parameter to __init__ method.
    - Update load method to handle both file and in-memory data.
    - Modify save method for in-memory data persistence.
    - Ensure JSON extension validation works with new logic.
    ```

--- utils/database.py
+++ utils/database.py
@@ -1,4 +1,5 @@
 import json
+import io
 from pathlib import Path
 
 from utils.classes import Namespace, Singleton
@@ -7,25 +8,40 @@ from utils.classes import Namespace, Singleton
 class Database(Singleton):
 	data: Namespace = Namespace()
 
-	def __init__(self, path: Path = Path.cwd().joinpath('db.json')):
+	def __init__(self, path: Path = Path.cwd().joinpath('db.json'), persistent: bool = False):
 		super().__init__()
 		self._path = path
+		self._persistent = persistent
+		if not self._persistent:
+			self._data_object = io.StringIO()
+
 		self.load()
 
-		if not self._path.suffix == '.json':
+		if not self._path.suffix == '.json' and not persistent:
 			raise ValueError(f"Database file must have .json extension, got {self._path.suffix}")
 
+	@classmethod
+	def _load_string(cls, string: str):
+		cls.data = Namespace(**json.loads(string or '{}'))
 	@classmethod
 	@Singleton.exists
 	def load(cls):
 		self = cls._instance
+
+		if not self._persistent:
+			self._data_object.seek(0)
+			cls._load_string(self._data_object.read())
+			return
 		if not self._path.exists():
 			cls.save()
 		with open(self._path, 'r') as f:
-			cls.data = Namespace(**json.loads(f.read()))
+			cls._load_string(f.read())
 
 	@classmethod
 	def save(cls):
 		self = cls._instance
+		if not self._persistent:
+			self._data_object.write(json.dumps(cls.data.dict()))
+			return
 		with open(self._path, 'w') as f:
 			f.write(json.dumps(cls.data.dict(), indent=2))
